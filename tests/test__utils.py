1e228e39205f9a209f91316fa8feef372757d43b
JGoutin <JGoutin@users.noreply.github.com>
2017-06-26 21:30:26 +0200

    Space

--- tests/test__utils.py
+++ tests/test__utils.py
@@ -1,15 +1,5 @@
 # -*- coding: utf-8 -*-
 """Tests for generic utilities"""
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import os
-assert os.getcwd() is None
-
->>>>>>> a25a744... Update test__utils.py
-=======
->>>>>>> 2d8b244... Update test__utils.py
-
 from compilertools._utils import always_str_list, import_class, BaseClass
 
 
