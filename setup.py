6f8e1cd4503b1c2da0eba2805b22d0409d844e77
JGoutin <ginnungagap@free.fr>
2018-05-25 21:00:15 +0200

    Update sphinx en setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 """compilertools setup"""
-from setuptools import setup, find_packages
 from datetime import datetime
+from os.path import abspath, dirname, join
 
 # Set Package information
-PACKAGE_INFOS = dict(
+PACKAGE_INFO = dict(
     name='compilertools',
     description='A library for helping optimizing Python extensions compilation.',
     long_description_content_type='text/markdown; charset=UTF-8',
@@ -23,38 +23,50 @@ PACKAGE_INFOS = dict(
     author='J.Goutin',
     url='https://github.com/JGoutin/compilertools',
     license='BSD',
-    packages=find_packages(exclude=['tests', 'doc']),
     zip_safe=True,
     python_requires='>=3.4',
-    extras_require={
-        'tests': ['pytest'],
-        'build_doc': ['sphinx']},
+    setup_requires=['setuptools'],
+    tests_require=['pytest'],
     command_options={},
     )
 
+SETUP_DIR = abspath(dirname(__file__))
+
 # Get package __version__ from package
-with open('compilertools/_version.py') as file:
+with open(join(SETUP_DIR, 'compilertools/_version.py')) as file:
     for line in file:
         if line.rstrip().startswith('__version__'):
-            PACKAGE_INFOS['version'] = line.split('=', 1)[1].strip(" \"\'\n")
+            PACKAGE_INFO['version'] = line.split('=', 1)[1].strip(" \"\'\n")
             break
 
 # Get long description from readme
-with open('readme.md') as file:
-    PACKAGE_INFOS['long_description'] = file.read()
+with open(join(SETUP_DIR, 'readme.md')) as file:
+    PACKAGE_INFO['long_description'] = file.read()
 
 # Sphinx configuration
-PACKAGE_INFOS['command_options']['build_sphinx'] = {
-    'project': ('setup.py', PACKAGE_INFOS['name'].capitalize()),
-    'version': ('setup.py', PACKAGE_INFOS['version']),
-    'release': ('setup.py', PACKAGE_INFOS['version']),
+PACKAGE_INFO['command_options']['build_sphinx'] = {
+    'project': ('setup.py', PACKAGE_INFO['name'].capitalize()),
+    'version': ('setup.py', PACKAGE_INFO['version']),
+    'release': ('setup.py', PACKAGE_INFO['version']),
     'copyright': ('setup.py', '2017-%s, %s' % (
-        datetime.now().year, PACKAGE_INFOS['author'])),
+        datetime.now().year, PACKAGE_INFO['author'])),
     }
 
 # Run setup
 if __name__ == '__main__':
     from os import chdir
-    from os.path import dirname, abspath
-    chdir(abspath(dirname(__file__)))
-    setup(**PACKAGE_INFOS)
+    from sys import argv
+    from setuptools import setup, find_packages
+
+    # Add pytest_runner requirement if needed
+    if {'pytest', 'test', 'ptr'}.intersection(argv):
+        PACKAGE_INFO['setup_requires'].append('pytest-runner')
+
+    # Add Sphinx requirements if needed
+    elif 'build_sphinx' in argv:
+        PACKAGE_INFO['setup_requires'] += ['sphinx', 'sphinx_rtd_theme']
+
+    # Run setup
+    chdir(SETUP_DIR)
+    setup(packages=find_packages(exclude=['tests', 'doc']),
+          **PACKAGE_INFO)
